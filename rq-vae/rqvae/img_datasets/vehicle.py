1a914ae960dce48c0e650b3faf632fd4059cb800
RickGao <rickgaoid@outlook.com>
2025-11-22 04:26:51 -0500

    rq-vae

---
+++ rq-vae/rqvae/img_datasets/vehicle.py
@@ -0,0 +1,55 @@
+# rqvae/img_datasets/vehicle.py
+import os
+from glob import glob
+from PIL import Image
+from torch.utils.data import Dataset
+import torch
+
+IMG_EXTS = ('.jpg', '.jpeg', '.JPG', '.JPEG', '.png', '.PNG')
+
+
+class Vehicle(Dataset):
+    """
+    Vehicle Dataset for image reconstruction tasks.
+    """
+
+    def __init__(self, root, split='train', transform=None, max_samples=None):
+        self.root = root
+        self.split = split
+        self.transform = transform
+
+        split_dir = os.path.join(root, split)
+
+        self.files = glob(os.path.join(split_dir, '**', '*.*'), recursive=True)
+        self.files = [f for f in self.files if f.lower().endswith(('.jpg', '.jpeg', '.png'))]
+        self.files.sort()
+
+        if len(self.files) == 0:
+            raise RuntimeError(f'No images found in {split_dir}')
+
+        # Limit Samples
+        if max_samples is not None and max_samples > 0:
+            self.files = self.files[:max_samples]
+            print(f'Limited to {len(self.files)} images in {split_dir}')
+        else:
+            print(f'Found {len(self.files)} images in {split_dir}')
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, idx):
+        img_path = self.files[idx]
+
+        # Load and convert to RGB
+        img = Image.open(img_path).convert('RGB')
+
+        # Apply transforms
+        if self.transform is not None:
+            img = self.transform(img)
+
+        # Return tuple (image, dummy_label) to match other datasets
+        # The trainer expects inputs[0] to be the image
+        return (img, 0)
+
+    def get_image_path(self, idx):
+        return self.files[idx]
